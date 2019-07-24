5c2ad986c95518f37d5e65aacf087bab317ed958
EC2 Default User <ec2-user@ip-172-31-23-223.us-west-2.compute.internal>
2019-07-24 20:14:48 +0000

    Admin now can select tech for detailed view

--- client/src/components/user_comps/SelectedTech.js
+++ client/src/components/user_comps/SelectedTech.js
@@ -5,47 +5,42 @@ import {Link} from 'react-router-dom';
 
 class UserView extends React.Component {
   
-    state={status: this.props.store.auth.status}
+    state={selectedUser: null}
     
     componentDidMount(){
-        this.props.getSelectedUser(window.location.href.split('/').slice(-1)[0])
-        console.log('USER ID is : ', window.location.href.split('/').slice(-1)[0])
-        console.log(this.props.store)
+        
     }
     
-    getColorOnStatus(){
-        switch(this.state.status){
-            case 'Running late':
-                return {marginTop:'15vh', border: "3px solid red", textAlign: "center"}
-            case 'Arrived to the site':
-                return {marginTop:'15vh', border: "3px solid #0ead0e3b", textAlign: "center"}
-            default :
-                return {marginTop:'15vh', border: "3px solid silver", textAlign: "center"}   
+   
+    renderTech=()=>{
+        const userId=   window.location.href.split('/').slice(-1)[0]
+        console.log('USER ID is : ', userId)
+        const selectedUser =   this.props.store.allUsers.find( (user)=>{return user._id===userId} )
+        const style = ()=>{
+            switch(selectedUser.status){
+                case 'Running late':
+                    return {marginTop:'15vh', border: "3px solid red", textAlign: "center"}
+                case 'Arrived to the site':
+                    return {marginTop:'15vh', border: "3px solid #0ead0e3b", textAlign: "center"}
+                default :
+                    return {marginTop:'15vh', border: "3px solid silver", textAlign: "center"}   
+            } 
         }
-    }
-    onStatusChange=async(e)=>{
-        const value= e.target.value
-        await this.setState({status:value})
-        await this.props.updateUserStatus(value)
-        console.log(this.state)
-    }
-    render(){
-        if(this.props.store.auth && this.props.store.auth.admin)
-        console.log(this.props.store.auth)
+        console.log(selectedUser)
         return(
-                 <div key={this.props.store.auth._id} className="card mx-auto" style={this.getColorOnStatus()}>
+                 <div key={selectedUser._id} className="card mx-auto" style={style()}>
                     <div className="btn-toolbar mb-3" role="toolbar" style={{ position: "fixed", right:"5px", top: "140px"  }}>
                         <div className="btn-group-vertical mr-2" role="group" aria-label="First group">
                            <Link to='/' className="btn btn-md btn-secondary">All</Link>
                         </div>
                     </div>
-                    <div className='btn btn-lg btn-outline-info'>{this.props.store.auth.projects.slice(-1)[0].projectStartTime} -- {this.props.store.auth.projects.slice(-1)[0].projectStartDate} </div>
-                      <h1>{this.props.store.auth.name}</h1>
+                    <div className='btn btn-lg btn-outline-info'>{selectedUser.projects.slice(-1)[0].projectStartTime} -- {selectedUser.projects.slice(-1)[0].projectStartDate} </div>
+                      <h1>{selectedUser.name}</h1>
                       <div className="card-body">
-                        <h4 className="card-title"><i class="fas fa-globe-americas"></i>{this.props.store.auth.region}</h4>
-                        <h4 >Project ID#:  {this.props.store.auth.projects.slice(-1)[0].projectId} </h4>
-                        <h5 >Project Name: {this.props.store.auth.projects.slice(-1)[0].projectName} </h5>
-                        <h5 className="card-text"><i class="fas fa-map-marked-alt"></i>{this.props.store.auth.installAddress}</h5>
+                        <h4 className="card-title"><i class="fas fa-globe-americas"></i>{selectedUser.region}</h4>
+                        <h4 >Project ID#:  {selectedUser.projects.slice(-1)[0].projectId} </h4>
+                        <h5 >Project Name: {selectedUser.projects.slice(-1)[0].projectName} </h5>
+                        <h5 className="card-text"><i class="fas fa-map-marked-alt"></i>{selectedUser.installAddress}</h5>
                         <form className='col-md-9 col-sm-9 col-lg-9 mx-auto'>
                           <div class="form-group">
                             Project ID: <input  class="form-control" id="projectId" />
@@ -64,32 +59,32 @@ class UserView extends React.Component {
                       </div>
                       
                       <ul className="list-group list-group-flush">
-                        <select 
-                        
-                            style = {this.getColorOnStatus() }
-                            className='form-control'
-                            onChange={this.onStatusChange}
-                            id="status"
-                            type="text"
-                            value={this.state.status}
-                                >
-                            <option value="Heading To site" >Heading To site</option>
-                            <option value="Arrived to the site">Arrived to the site</option>
-                            <option value="Running late">Running late</option>
-                        </select>
-                        <li className="list-group-item">lat: {this.props.store.auth.lat}, lng: {this.props.store.auth.lng}</li>
-                        <li className="list-group-item"><i class="fas fa-phone"></i> : {this.props.store.auth.phone}</li>
-                        <li className="list-group-item"><i class="fas fa-envelope-open"></i>: {this.props.store.auth.email}</li>
+                        <li className="list-group-item" ><b> Arrival Status:  {selectedUser.status} </b></li>
+                        <li className="list-group-item">lat: {selectedUser.lat}, lng: {selectedUser.lng}</li>
+                        <li className="list-group-item"><i class="fas fa-phone"></i> : {selectedUser.phone}</li>
+                        <li className="list-group-item"><i class="fas fa-envelope-open"></i>: {selectedUser.email}</li>
                       </ul>
                       <div className="card-body">
-                        <a href={`mailto: myPM@commscope.com?Subject=${'Issue with install : '}${this.props.store.auth.projectId}${this.props.store.auth.projectName}` } className="card-link"> Commscope Manager: <i class="far fa-envelope-open"></i> </a>
-                        <a href={`mailto: ${this.props.store.auth.directManagerEmail}?Subject=${'Issue with install : '}${this.props.store.auth.projectId}${this.props.store.auth.projectName}` } className="card-link">
-                            Direct Manager: <i class="far fa-envelope-open"></i> {this.props.store.auth.directManagerName}</a>
+                        <a href={`mailto: myPM@commscope.com?Subject=${'Issue with install : '}${selectedUser.projectId}${selectedUser.projectName}` } className="card-link"> Commscope Manager: <i class="far fa-envelope-open"></i> </a>
+                        <a href={`mailto: ${selectedUser.directManagerEmail}?Subject=${'Issue with install : '}${selectedUser.projectId}${selectedUser.projectName}` } className="card-link">
+                            Direct Manager: <i class="far fa-envelope-open"></i> {selectedUser.directManagerName}</a>
                       </div>
                 </div>
           
             
             )
+    }
+    
+    onStatusChange=async(e)=>{
+        const value= e.target.value
+        await this.setState({status:value})
+        await this.props.updateUserStatus(value)
+        console.log(this.state)
+    }
+    render(){
+        if(this.props.store.auth.admin)
+            return(this.renderTech())
+            
     }
 }
 
